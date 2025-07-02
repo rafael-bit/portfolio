14767f08336a15b4df7d48a5d0fe5f1cd0ca8e93
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2025-07-02 16:10:47 -0300

    fix; error build

--- next.config.js
+++ next.config.js
@@ -14,34 +14,6 @@ const nextConfig = {
     ],
   },
   trailingSlash: false,
-  experimental: {
-    optimizeCss: true,
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-          {
-            key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
